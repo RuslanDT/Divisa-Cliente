86abb9d8022fb9ea402d6abe075b35b7c175f7dd
Ruslan <s19120135@alumnos.itsur.edu.mx>
2023-03-09 23:39:25 -0600

    Se agrego interfaz y provider

--- app/src/main/java/com/example/divisacliente/MainActivity.kt
+++ app/src/main/java/com/example/divisacliente/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.divisacliente
 
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SimpleCursorAdapter
+import android.widget.Spinner
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        spnMonedas = findViewById(R.id.spinner)
+
+        LoaderManager.getInstance(this)
+            .initLoader<Cursor>(1001, null, mLoaderCallbacks)
+    }
+
+    lateinit var spnMonedas : Spinner
+
+    val mLoaderCallbacks = object : LoaderManager.LoaderCallbacks<Cursor>{
+        override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+            return CursorLoader(
+                applicationContext,
+                Uri.parse("content://com.example.divisaserver/cambios"),
+                arrayOf<String>("_ID", "codeMonedaCambio","cambio","fechaActualizacion","fechaConsulta"),
+                null, null, null)
+        }
+
+        override fun onLoaderReset(loader: Loader<Cursor>) {}
+
+        override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+            val adapter = SimpleCursorAdapter(applicationContext,
+                R.layout.moneda_item,data,
+                arrayOf<String>("_ID","cambio","codeMonedaCambio","fechaActualizacion","fechaConsulta"),
+                IntArray(5).apply {
+                    set(1, R.id.text1)
+                    set(2, R.id.text2)
+                    set(3, R.id.text3)
+                    set(4, R.id.text4)
+                },
+                SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE
+            )
+            spnMonedas.adapter =adapter
+        }
     }
 }
